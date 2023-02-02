402a10d7c84f12df09d06b333f7e935bb08f3bbd
Илья Ураев <uraevilya@yandex.ru>
2023-02-02 11:22:20 +0300

    fixt after first rew. in Docker settings compose files

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,11 +17,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'django_filters',
+    'rest_framework',
     'api',
+    'reviews',
     'users',
-    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
